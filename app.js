278deb62bf2d2d4680e4b4020c89eb68e096bd71
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-09-12 17:14:03 -0400

    refactored the whole project for the buttons

--- app.js
+++ app.js
@@ -1,43 +1,17 @@
+const rockButton = document.getElementById('rock');
+const scissorsButton = document.getElementById('scissors');
+const paperButton = document.getElementById('paper');
+const choiceButtons = document.querySelectorAll('button.choices');
+
 let humanScore = 0;
 let computerScore = 0;
 
-const getHumanChoice = () => {
-    let messagePrompt = prompt("Please choose rock, paper, or scissors");
-    
-    if (messagePrompt.toLowerCase() === 'rock' | messagePrompt.toLowerCase() === 'paper' | messagePrompt.toLowerCase() === 'scissors') {
-        const humanChoice = messagePrompt.toLowerCase();
-        return humanChoice;
-    } else {
-        alert('Please enter a valid response.')
-        getHumanChoice();
-    }
-};
+const getHumanChoice = () => {};
 
-const getComputerChoice = () => {
-    let choiceArray = ['rock', 'paper', 'scissors'];
-    let computerChoice = Math.floor(Math.random()*choiceArray.length);
+const getComputerChoice = () => {};
 
-    return choiceArray[computerChoice];
-};
+const playRound = () => {};
 
-const playerWins = (player, computer) => {
-    return (
-        (player === 'rock' & computer === 'scissors') ||
-        (player === 'scissors' & computer === 'paper') ||
-        (player === 'paper' & computer === 'rock')
-    );
-};
-
-const playRound = (humanChoice, computerChoice) => {
-    if (playerWins(humanChoice, computerChoice)) {
-        humanScore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    } else if (humanChoice === computerChoice) {
-        console.log(`It's a tie!`);
-    } else {
-        computerScore++;
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    }
-};
-
-
+choiceButtons.addEventListener('click', () => {
+    playRound()
+};);
